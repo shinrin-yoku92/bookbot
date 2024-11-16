4e59b7d59335addc2e0d0bf903558ba8d5360e4d
jfeast <jordan.feast@outlook.com>
2024-11-16 21:05:11 +0000

    added feature: word count

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
+    word_count = 0
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+        print(file_contents)
     
+    words = file_contents.split()
+
+    for word in words:
+        word_count += 1
+
+    print(word_count)
+
 main()
