9bb2b87009c037367a3d8684634d29716cb9f83a
jfeast <jordan.feast@outlook.com>
2024-11-16 22:27:33 +0000

    added feature: letter count

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 def main():
-    word_count = 0
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
+    print(f"{num_words} words found in the document")
+    print(num_letters)
 
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    
-    words = file_contents.split()
 
-    for word in words:
-        word_count += 1
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-    print(word_count)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_letters(text):
+    lowered_string = text.lower()
+    letter_count = {}
+    for letter in lowered_string:
+        if letter.isalpha():
+            if letter not in letter_count:
+                letter_count[letter] = 1
+            else:
+                letter_count[letter] += 1
+    return letter_count
+
 
 main()
